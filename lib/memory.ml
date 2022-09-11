e4a97478a79aa665597505a025fe2febdc1dd5d6
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2022-09-12 01:02:25 +0900

    dune fmt した

--- lib/memory.ml
+++ lib/memory.ml
@@ -1,9 +1,7 @@
-type t = { bytes: Bytes.t }
+type t = { bytes : Bytes.t }
 
 let create n = { bytes = Bytes.create n }
-
 let make n c = { bytes = Bytes.make n c }
-
 let of_string str = { bytes = Bytes.of_string str }
 
 let read_word memory addr =
@@ -20,7 +18,7 @@ let write_word memory addr word =
   and b1 = (wd land 0xff00) lsr 8
   and b2 = (wd land 0xff0000) lsr 16
   and b3 = (wd land 0xff000000) lsr 24 in
-  (ignore(Bytes.set_uint8 memory.bytes (addr + 3) b3);
-   ignore(Bytes.set_uint8 memory.bytes (addr + 2) b2);
-   ignore(Bytes.set_uint8 memory.bytes (addr + 1) b1);
-   ignore(Bytes.set_uint8 memory.bytes (addr + 0) b0))
+  ignore (Bytes.set_uint8 memory.bytes (addr + 3) b3);
+  ignore (Bytes.set_uint8 memory.bytes (addr + 2) b2);
+  ignore (Bytes.set_uint8 memory.bytes (addr + 1) b1);
+  ignore (Bytes.set_uint8 memory.bytes (addr + 0) b0)
--- test/memory_test.ml
+++ test/memory_test.ml
@@ -8,9 +8,15 @@ let test = Rvsim.Memory.read_word memory 4
 let _ = Printf.printf "%08x\n" (Int32.to_int test)
 
 (* アドレス0番地へ書き込み *)
-let test = (Rvsim.Memory.write_word memory 0 0x00ABCDEFl; Rvsim.Memory.read_word memory 0)
+let test =
+  Rvsim.Memory.write_word memory 0 0x00ABCDEFl;
+  Rvsim.Memory.read_word memory 0
+
 let _ = Printf.printf "%08x\n" (Int32.to_int test)
 
 (* アドレス4番地へ書き込み *)
-let test = (Rvsim.Memory.write_word memory 4 (-1l); Rvsim.Memory.read_word memory 4)
+let test =
+  Rvsim.Memory.write_word memory 4 (-1l);
+  Rvsim.Memory.read_word memory 4
+
 let _ = Printf.printf "%d\n" (Int32.to_int test)
